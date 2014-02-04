e3d3b88192d8ba7b4a7abbcaf7e2f63a08b4af37
Syed Saifullah Albiz <ssalbiz@gmail.com>
2014-02-04 00:08:08 -0500

    initial commit

---
+++ tumblr-image.go
@@ -0,0 +1,67 @@
+package main
+import (
+  "encoding/xml"
+  "net/http"
+  "log"
+  "flag"
+  "fmt"
+  "regexp"
+)
+
+type Item struct {
+  Title string `xml:"title"`
+  Description string `xml:"description"`
+  Guid string `xml:"guid"`
+  PubDate string `xml:"pubDate"`
+  Category []string `xml:"category"`
+}
+
+type NoSnippet struct {}
+
+func (ns NoSnippet) Error() string {
+  return "No snippet found"
+}
+
+var target = flag.String("rss", "", "tumblr rss feed url to poll")
+
+func GetSnippet(htmlblob string) (string, error) {
+  re := regexp.MustCompile("<img src=\"(.*)\"/>");
+  results := re.FindStringSubmatch(htmlblob)
+  if results == nil || len(results) < 2 {
+    return "", NoSnippet{}
+  }
+  return results[1], nil
+}
+
+func main() {
+  flag.Parse()
+  rss, err := http.Get(*target)
+  if err != nil {
+    log.Fatal("Error retrieving rss feed: ", err)
+  }
+  defer rss.Body.Close()
+
+  decoder := xml.NewDecoder(rss.Body)
+  for {
+    t, _ := decoder.Token()
+    if t == nil {
+      break
+    }
+    switch se := t.(type) {
+      case xml.StartElement:
+        var decoded Item
+        if se.Name.Local == "item" {
+          err = decoder.DecodeElement(&decoded, &se)
+          if err != nil {
+            log.Fatal("Error parsing rss xml: ", err)
+          }
+          img, err := GetSnippet(decoded.Description)
+          if err != nil {
+            log.Fatal("Error grabbing image out of xml blob: ", err)
+          }
+          fmt.Println(img)
+          return;
+        }
+    }
+  }
+}
